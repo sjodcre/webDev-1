1f3b68672d71bb0c68b8a988232380ee256aca33
Samuel Omamalin <samuelomamalin@gmail.com>
2019-03-15 03:43:33 +0000

    RESTful route 4 done

--- app.js
+++ app.js
@@ -61,12 +61,13 @@ app.get("/campgrounds", function(req, res){
 app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image};
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description:desc};
     Campground.create(newCampground,function(err,campground){
         if(err){
             console.log(err)
         } else {
-            res.redirect("/index");
+            res.redirect("/campgrounds");
         }
     })
     
