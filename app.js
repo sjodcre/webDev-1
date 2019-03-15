c68123ebf8dccaa288f5ce3980fb94f6584b26d1
Samuel Omamalin <samuelomamalin@gmail.com>
2019-03-15 03:34:41 +0000

    adding 4th REST api route

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser")
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose")
 
+mongoose.connect("mongodb://localhost:27017/yelp_camp",{useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 
+
+//schema
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+})
+
+var Campground = mongoose.model("Campground",campgroundSchema);
+
+
+// Campground.create(
+//     {
+//         name: "Campfire",
+//         image: "https://pixabay.com/get/e83db7082af3043ed1584d05fb1d4e97e07ee3d21cac104490f2c470a1e4b3bf_340.jpg",
+//         description: "dajdbsadihdisadaidjiasdsdjisdjisdjsidjsdisjdidsjdidjdsjdisjdiadjidjidjidji"
+        
+//     }, function(err,campground){
+//         if(err){
+//             console.log(err);
+//         } else {
+//             console.log("newly created cg:");
+//             console.log(campground);
+            
+//         }
+//     });
+    
 var campgrounds = [
     {name: "test", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104490f2c478a1e5b6bd_340.jpg"},
     {name: "test1", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104490f2c478a1e5b6bd_340.jpg"},
@@ -18,23 +47,48 @@ app.get("/",function(req,res){
     res.render("landing");
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Server started!");
-});
 
 app.get("/campgrounds", function(req, res){
-
-    res.render("campgrounds", {campgrounds:campgrounds});
+    Campground.find({},function(err,campgrounds){
+        if(err){
+            console.log(err)
+        } else{
+            res.render("index", {campgrounds:campgrounds})
+        }
+    })
 });
 
 app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
+    Campground.create(newCampground,function(err,campground){
+        if(err){
+            console.log(err)
+        } else {
+            res.redirect("/index");
+        }
+    })
+    
 });
 
 app.get("/campgrounds/new", function(req, res) {
   res.render("new");  
+});
+
+app.get("/campgrounds/:id",function(req, res){
+    
+    Campground.findById(req.params.id, function(err, foundCampground){
+       if(err){
+           console.log(err)
+       } else {
+           res.render("show", {campground:foundCampground});
+       }
+    });
+     
+});
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server started!");
 });
