d066a211475e940ef08e20a6b42bb508cfee37a3
Samuel Omamalin <samuelomamalin@gmail.com>
2019-03-19 05:16:58 +0000

    add landing, updates, and start script

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -17,12 +17,13 @@ router.get("/", function(req, res){
 router.post("/", middleware.isLoggedIn, function(req, res){
     var name = req.body.name;
     var image = req.body.image;
+    var price = req.body.price;
     var desc = req.body.description;
     var author = {
         id:req.user._id,
         username: req.user.username
     }
-    var newCampground = {name: name, image: image, description:desc, author:author};
+    var newCampground = {name: name, price:price, image: image, description:desc, author:author};
     Campground.create(newCampground,function(err,campground){
         if(err){
             console.log(err)
@@ -42,7 +43,7 @@ router.get("/:id",function(req, res){
     Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
        if(err || !foundCampground){
             req.flash("error","Campground not found");
-            res.redirect("back");
+            res.redirect("back");   
        } else {
            res.render("campgrounds/show", {campground:foundCampground});
        }
